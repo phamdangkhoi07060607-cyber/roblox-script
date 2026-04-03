48d41b1b825c0a77f80d36389b88af48c9e93a5f
blutbequite <phamdangkhoi07060607@gmail.com>
2026-04-03 14:17:47 +0700

    Update scripthack.lua

--- scripthack.lua
+++ scripthack.lua
@@ -1,22 +1,37 @@
--- Client ESP Part Name (ignore Part named "Part")
+local ignoreNames = {
+    Part = true,
+    Grass = true,
+    Trunk = true
+}
 
-for _, v in pairs(workspace:GetDescendants()) do
-    if v:IsA("Part") and v.Name ~= "Part" then
-        
-        local billboard = Instance.new("BillboardGui")
-        billboard.Name = "PartESP"
-        billboard.Adornee = v
-        billboard.Parent = v
-        billboard.Size = UDim2.new(0,100,0,40)
-        billboard.StudsOffset = Vector3.new(0,2,0)
-        billboard.AlwaysOnTop = true
-        
-        local text = Instance.new("TextLabel")
-        text.Parent = billboard
-        text.Size = UDim2.new(1,0,1,0)
-        text.BackgroundTransparency = 1
-        text.Text = v.Name
-        text.TextColor3 = Color3.fromRGB(255,0,0)
-        text.TextScaled = true
-    end
+function createESP(v)
+    if not v:IsA("BasePart") then return end
+    if ignoreNames[v.Name] then return end
+    if v:FindFirstChild("PartESP") then return end
+
+    local billboard = Instance.new("BillboardGui")
+    billboard.Name = "PartESP"
+    billboard.Adornee = v
+    billboard.Parent = v
+    billboard.Size = UDim2.new(0,100,0,40)
+    billboard.StudsOffset = Vector3.new(0,2,0)
+    billboard.AlwaysOnTop = true
+
+    local text = Instance.new("TextLabel")
+    text.Parent = billboard
+    text.Size = UDim2.new(1,0,1,0)
+    text.BackgroundTransparency = 1
+    text.Text = v.Name
+    text.TextColor3 = Color3.fromRGB(255,0,0)
+    text.TextScaled = true
 end
+
+-- ESP cho part đã có
+for _,v in pairs(workspace:GetDescendants()) do
+    createESP(v)
+end
+
+-- ESP cho part mới spawn
+workspace.DescendantAdded:Connect(function(v)
+    createESP(v)
+end)
