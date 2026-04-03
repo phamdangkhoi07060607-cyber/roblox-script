ae53041796c3c03d821a3bb7236764758a995a11
blutbequite <phamdangkhoi07060607@gmail.com>
2026-04-03 14:13:45 +0700

    Update scripthack.lua

--- scripthack.lua
+++ scripthack.lua
@@ -1,8 +1,11 @@
--- Client ESP Part Name
+-- Client ESP Part Name (ignore Part named "Part")
+
 for _, v in pairs(workspace:GetDescendants()) do
-    if v:IsA("Part") then
+    if v:IsA("Part") and v.Name ~= "Part" then
         
         local billboard = Instance.new("BillboardGui")
+        billboard.Name = "PartESP"
+        billboard.Adornee = v
         billboard.Parent = v
         billboard.Size = UDim2.new(0,100,0,40)
         billboard.StudsOffset = Vector3.new(0,2,0)
@@ -13,7 +16,7 @@ for _, v in pairs(workspace:GetDescendants()) do
         text.Size = UDim2.new(1,0,1,0)
         text.BackgroundTransparency = 1
         text.Text = v.Name
-        text.TextColor3 = Color3.new(1,0,0)
+        text.TextColor3 = Color3.fromRGB(255,0,0)
         text.TextScaled = true
     end
 end
